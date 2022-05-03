33a0ae651be30f47216e0a31d4c1c5027f210534
unknown <daria_lash@naumenko-daria.pro>
2022-05-03 17:02:35 +0300

    CORS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -118,13 +118,13 @@ class App extends React.Component {
     }
 
     load_data() {
-        //const headers = this.get_headers()
-        axios.get('http://151.248.125.11:8080/api/users/')
+        const headers = this.get_headers()
+        axios.get('http://151.248.125.11:8080/api/users/', {headers})
             .then(response => {
                 this.setState({users: response.data})
             }).catch(error => console.log(error))
 
-        axios.get('http://151.248.125.11:8080/api/projects/')
+        axios.get('http://151.248.125.11:8080/api/projects/', {headers})
             .then(response => {
                 this.setState({
                     projects: response.data,
@@ -132,7 +132,7 @@ class App extends React.Component {
                 })
             }).catch(error => console.log(error))
 
-        axios.get('http://151.248.125.11:8080/api/todos/')
+        axios.get('http://151.248.125.11:8080/api/todos/', {headers})
             .then(response => {
                 this.setState({todos: response.data})
             }).catch(error => {
--- main/admin.py
+++ main/admin.py
@@ -5,10 +5,11 @@ from main.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'email', 'is_active', 'is_superuser',
+    list_display = ('username', 'password', 'first_name', 'last_name', 'email', 'is_active', 'is_superuser',
                     'is_staff', 'date_joined')
     fields = (
         'username',
+        'password',
         'email', ('first_name', 'last_name'),
         'uid',
         ('is_superuser', 'is_staff', 'is_active'),
